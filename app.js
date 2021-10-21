09830e3533189ed5615d3771bb4099c7cbdaf880
Denys Sliusarenko <Denys.Sliusarenko@bd.com>
2021-10-21 17:14:29 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const greeting = require("./greeting");
+const os = require("os");
+const User = require("./user.js");
+
+let userName = os.userInfo().username;
+console.log(`Date: ${greeting.date}`);
+console.log(greeting.getMessage(userName));
+
+ 
+let denys = new User("Denys", 24);
+denys.sayHi();
+
+http.createServer(function(request,response){
+
+    response.end("Hello NodeJS!");
+
+}).listen(3000, "127.0.0.1",function(){
+    console.log("Server is listening on port 3000");
+});

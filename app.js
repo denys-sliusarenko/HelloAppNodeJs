49ff66e0f0fd7b66bea3ba41b7e86542e1b7e058
Denys Sliusarenko <Denys.Sliusarenko@bd.com>
2021-10-22 12:57:21 +0300

    add modules

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require("http");
 const greeting = require("./greeting");
 const os = require("os");
 const User = require("./user.js");
+const welcome = require("./welcome");//folder
 
 let userName = os.userInfo().username;
 console.log(`Date: ${greeting.date}`);
@@ -18,3 +19,7 @@ http.createServer(function(request,response){
 }).listen(3000, "127.0.0.1",function(){
     console.log("Server is listening on port 3000");
 });
+
+ 
+welcome.getMorningMessage();
+welcome.getEveningMessage();
